04027f3b271388f3915c3aa0429c8b9b22a37001
___Jun <27885056@qq.com>
2022-01-21 15:33:16 +0800

    init

---
+++ src/AppServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Jun\LaravelBatch;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+/**
+ * @method update(array $array)
+ */
+class AppServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function register(): void
+    {
+
+    }
+}
